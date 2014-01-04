dc4489656321055c349712d198211246583ee6e3
carlo scarioni <carlo.scarioni@gmail.com>
2014-01-04 01:33:30 +0000

    second commit

---
+++ Assets/scripts/Bucket.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bucket : MonoBehaviour {
+	
+  public float Speed = 10.0f;
+  public Vector2 MinMaxX = Vector2.zero;
+
+  void Update ()
+  {
+    transform.position = new Vector3(Mathf.Clamp(transform.position.x + Input.GetAxis("Horizontal") * Speed * Time.deltaTime, MinMaxX.x, MinMaxX.y), transform.position.y,
+		                                 transform.position.z);
+  }
+}
