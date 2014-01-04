ceb6930b3545fe04b4825990c24304150ce0a337
carlo scarioni <carlo.scarioni@gmail.com>
2014-01-04 01:18:46 +0000

    first commit

---
+++ Assets/scripts/Bomb.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bomb : MonoBehaviour {
+	public Vector3 Direction = Vector3.down;
+
+	public float Speed = 20.0f;
+
+	public float Lifetime = 10.0f;
+
+	void Start ()
+	{
+		Invoke("DestroyMe", Lifetime);
+	}
+
+	void Update ()
+	{
+		transform.position += Direction * Speed * Time.deltaTime;
+	}
+
+	void DestroyMe()
+	{
+		Destroy(gameObject);
+	}
+}
---
+++ Assets/scripts/ShipBehaviour.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShipBehaviour : MonoBehaviour {
+	public float Speed = 10.0f;
+    public Vector2 MinMaxX = Vector2.zero;
+	public GameObject PrefabAmmo = null;
+	public GameObject GunPosition = null;
+
+	private int updateCount=0;
+	private int direction=1;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		updateCount++;
+		if(updateCount % 15 == 0){
+		  do{
+		      direction = Random.Range(-1, 2);
+			}while(direction == 0);
+			Instantiate(PrefabAmmo, GunPosition.transform.position, PrefabAmmo.transform.rotation);
+		}
+		transform.position = new Vector3(Mathf.Clamp(transform.position.x + direction * Speed * Time.deltaTime, MinMaxX.x, MinMaxX.y), transform.position.y, transform.position.z);
+	}
+}
